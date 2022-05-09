8a22a71407018abd06d8a1e06756e85694d5d7d7
SantiR <santiago.r.n@hotmail.com>
2022-05-09 13:43:42 -0300

    filtrado alfabetico a/z

--- pokedex/src/components/Api/Pokemon.jsx
+++ pokedex/src/components/Api/Pokemon.jsx
@@ -1,36 +1,52 @@
-import { Usefetch } from '../Api/Usefetch'
-import { useEffect, useState } from 'react'
-import { Card } from '../Card/Card'
-import Buscador from '../Nav/buscador'
-import './pokemon.css'
+import { Usefetch } from "../Api/Usefetch";
+import { useEffect, useState } from "react";
+import { Card } from "../Card/Card";
+import Buscador from "../Nav/buscador";
+import "./pokemon.css";
+
 // Este componente representa la pagina con el listado
 
 const Pokemon = () => {
-    const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/')
-    const pokemons = Usefetch(url) //estado tiene como valor el objeto que nos devuelve Usefetch con sus atributos
-    const {loading, data} = pokemons;
-    loading? console.log('cargando') : console.log(pokemons)
-    let pokemones = data
-    // Aca comprobamos que esta funcionando en la consola
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+  const estado = Usefetch(url); //estado tiene como valor el objeto que nos devuelve Usefetch con sus atributos
+  const { loading, data } = estado;
+  //   loading ? console.log("cargando") : console.log(data.results);
+  const [pokemon, setPokemon] = useState(data);
+  // Aca comprobamos que esta funcionando en la consola
 
-    const onClicksort = () => {
-        // const neWpokemones = data.results.sort()
-        data.results.sort((a,b) => a.name - b.name);
-        console.log(data)
-        // pokemones = neWpokemones
+  useEffect(() => {
+    if (data) {
+      setPokemon(data.results);
     }
-    
-    return (
-        <div>
-            <button onClick={onClicksort} className='button' >Ordenar</button>
-            <Buscador/>
-            { loading? <h1>Cargando...</h1> : <Card results= {data.results} /> }
+  }, [data]);
 
-            <div className='button-contain'>
-                <button onClick={()=>setUrl(data.previous)} className='button' >Anterior</button>
-                <button onClick={()=>setUrl(data.next)} className='button' >Siguiente</button>
-            </div>
-        </div>
-    )
-}
-export default Pokemon
+  const onClickTest = () => {
+    const pokemonClone = [...pokemon];
+
+    pokemonClone.sort((a, b) =>
+      a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
+    );
+
+    setPokemon(pokemonClone);
+  };
+
+  return (
+    <div>
+      <button onClick={onClickTest} className="button">
+        Ordenar
+      </button>
+      <Buscador />
+      {loading ? <h1>Cargando...</h1> : <Card results={pokemon} />}
+
+      <div className="button-contain">
+        <button onClick={() => setUrl(data.previous)} className="button">
+          Anterior
+        </button>
+        <button onClick={() => setUrl(data.next)} className="button">
+          Siguiente
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Pokemon;
